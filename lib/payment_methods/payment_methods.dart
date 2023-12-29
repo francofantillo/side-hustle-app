82c1da8276cbfc07d74c515d220bf5a839b83ed8
Syed Hassan <hassan.baqar@eplanetglobal.com>
2023-12-29 20:08:21 +0500

    wanted jobs

--- lib/payment_methods/payment_methods.dart
+++ lib/payment_methods/payment_methods.dart
@@ -67,9 +67,6 @@ class _ManagePaymentMethodsState extends State<ManagePaymentMethods> {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     PaymentList(),
-                    Padding(
-                      padding: EdgeInsets.only(left: 8.0, right: 8, top: 8),
-                    )
                   ],
                 ),
               );
